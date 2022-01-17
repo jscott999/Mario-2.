b399bf32dfc677d7f345ad890506ac75ff9ab644
justinscott <jscoot9999@gmil.com>
2022-01-17 09:36:40 -0600

    ADDING TO THE REWORK

--- src/main/java/JADE/Window.java
+++ src/main/java/JADE/Window.java
@@ -2,15 +2,18 @@ package JADE;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryUtil;
 
 import java.util.jar.JarFile;
 
-import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
-import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class Window {
+    private static final int GLFE_MAXIMIZED = ;
     private int width, height;
     private String title;
+    private long glfwWindow;
 
     private static Window window = null;
     private JarFile Version;
@@ -47,7 +50,29 @@ public class Window {
         
         // Configure GLFW
         glfwDefaultWindowHints();
-        glfwDefaultWindowHint(GLFW_VSIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFE_MAXIMIZED, GLFW_TRUE);
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, MemoryUtil.NULL, MemoryUtil.NULL);
+        if (glfwWindow == MemoryUtil.NULL) {
+            throw new IllegalStateException("Failed to vreate GLFW window.");
+        }
+
+        // make the OpenGl context current
+        glfwMakeContextCurrent(glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        //Make window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        //OpenGL context, or any context that is managed externally.
+        //LWJGL detects the context that is current in the current thread,
+        //bindings available to use.
+        GL.createCapabilities();
     }
 
     private boolean glfwinit() {
