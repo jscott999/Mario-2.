c192dbbbe839175006f3caf931cdc8fba834095d
justinscott <jscoot9999@gmil.com>
2022-01-16 23:17:21 -0600

    ADDING TO THE REWORK

--- src/main/java/JADE/Window.java
+++ src/main/java/JADE/Window.java
@@ -1,10 +1,19 @@
 package JADE;
 
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+
+import java.util.jar.JarFile;
+
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+
 public class Window {
     private int width, height;
     private String title;
 
-    private static Window window = null
+    private static Window window = null;
+    private JarFile Version;
 
     private Window() {
         this.width = 1920;
@@ -18,5 +27,33 @@ public class Window {
         }
 
         return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWGLJ" + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    public void init() {
+        // Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+        
+        // Initialize GLFW
+        if (!glfwinit()) {
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+        
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwDefaultWindowHint(GLFW_VSIBLE, GLFW_FALSE);
+    }
+
+    private boolean glfwinit() {
+    }
+
+    public void loop() {
+
     }
 }
